fa26848088e6c4fd7bd90dd6efce22eb2107328c
Kodewerfer <lijleen@gmail.com>
2024-06-17 20:30:35 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import {useRef} from "react";
+import "./App.css";
+import dedent from "dedent";
+import Editor, {TEditorForwardRef} from "react-magic-draft-md";
+
+const MarkdownFakeDateTwo = dedent`
+# Title!
+
+Normal text test one
+
+Normal text test two
+
+Normal text test three
+
+**custom**Normal text test
+
+**syntax**
+
+**custom1** **custom2** **custom3**
+
+ - list1**test**
+ - list2
+ - list3
+
+`;
+
+export default function Test_App() {
+    const EditorRef = useRef<TEditorForwardRef>(null);
+    
+    async function appClick() {
+        console.log("NOTAMARCK click");
+        if (EditorRef.current) {
+            console.log(await EditorRef.current.ExtractMD());
+        }
+    }
+    
+    return (
+        <div className="App">
+            <button className={"bg-amber-600"} onClick={appClick}>
+                NOTAMARCK EXTRACT
+            </button>
+            <main className="Main-wrapper">
+                <Editor SourceData={MarkdownFakeDateTwo} ref={EditorRef}/>
+            </main>
+        </div>
+    );
+}
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import {defineConfig} from 'vite'
+import path from 'node:path'
+import electron from 'vite-plugin-electron/simple'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        electron({
+            main: {
+                // Shortcut of `build.lib.entry`.
+                entry: 'electron/main.ts',
+                onstart(args) {
+                    if (process.env.VSCODE_DEBUG) {
+                        console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
+                    } else {
+                        args.startup([
+                            '.',
+                            '--no-sandbox',
+                            '--sourcemap',
+                            // For Chrome devtools
+                            '--remote-debugging-port=9222',
+                        ])
+                    }
+                },
+            },
+            preload: {
+                // Shortcut of `build.rollupOptions.input`.
+                // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+                input: path.join(__dirname, 'electron/preload.ts'),
+            },
+            // Ployfill the Electron and Node.js API for Renderer process.
+            // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+            // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+            renderer: process.env.NODE_ENV === 'test'
+                // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+                ? undefined
+                : {},
+        }),
+    ],
+})
