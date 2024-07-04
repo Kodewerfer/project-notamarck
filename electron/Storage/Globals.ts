89619fa815d12dec3be61db779b1867e815de4ce
Kodewerfer <lijleen@gmail.com>
2024-07-04 16:04:09 -0400

    Improved handling for adding and removing files in _Opened_Files array

--- electron/Storage/Globals.ts
+++ electron/Storage/Globals.ts
@@ -12,16 +12,31 @@ export function GetOpenedFiles(): ReadonlyArray<TOpenedFiles> {
   return [..._Opened_Files];
 }
 
-export function AddToOpenedFiles(FileInfo: TOpenedFiles) {
+export function FindInOpenedFiles(FileInfo: TOpenedFiles) {
+  return _Opened_Files.some((arrItem: TOpenedFiles) => arrItem.fullPath === FileInfo.fullPath);
+}
+
+export function AddToOpenedFiles(arr = [..._Opened_Files], FileInfo: TOpenedFiles) {
+  // already have the element
+  if (arr.some((arrItem: TOpenedFiles) => arrItem.fullPath === FileInfo.fullPath)) {
+    return _Opened_Files.length;
+  }
+
   // NOTE: shallow copy
   return _Opened_Files.push({ ...FileInfo });
 }
 
-export function RemoveOpenedFile(
-  arr = [..._Opened_Files],
-  item: TOpenedFiles,
-): ReadonlyArray<TOpenedFiles> {
-  const index = arr.indexOf(item);
+export function RemoveOpenedFile(arr = [..._Opened_Files], removingItem: TOpenedFiles): ReadonlyArray<TOpenedFiles> {
+  let index = -1;
+
+  arr.some((arrItem: TOpenedFiles, arrIndex) => {
+    if (arrItem.fullPath === removingItem.fullPath) {
+      index = arrIndex;
+      return true;
+    }
+    return false;
+  });
+
   index > -1 && arr.splice(index, 1);
   _Opened_Files = arr;
   return arr;
