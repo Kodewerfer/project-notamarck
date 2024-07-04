741fe45b52635653e33aa5103dd78c2b86a8f6b6
Kodewerfer <lijleen@gmail.com>
2024-07-04 14:23:51 -0400

    Empty editing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/')({
   component: EmptyEditor,
 });
 
 function EmptyEditor() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate({ to: '/edit' });
+  }, []);
+
   return (
     <>
-      <div> EMPTY</div>
+      <div> Index </div>
     </>
   );
 }
