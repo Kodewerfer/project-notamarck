6ccbe3039304542e8263b2e78bc37dc6320262cd
Kodewerfer <lijleen@gmail.com>
2024-07-13 21:44:11 -0400

    added empty component

---
+++ src/component/Settings.tsx
@@ -0,0 +1,9 @@
+export function Settings() {
+  return (
+    <>
+      <div className={'grid h-full w-full content-center justify-center'}>
+        <div className={'h-20 w-20 bg-blue-500'}>Setting inner</div>
+      </div>
+    </>
+  );
+}
