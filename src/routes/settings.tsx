0d0a6b39768371bfedd809ad9e022ff1b71bf34e
Kodewerfer <lijleen@gmail.com>
2024-07-15 15:51:25 -0400

    merged components since it will be non-reused

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Settings } from 'component/Settings.tsx';
 
 export const Route = createFileRoute('/settings')({
   component: Settings,
 });
+
+function Settings() {
+  return (
+    <>
+      <div className={'grid h-full w-full content-center justify-center bg-slate-800/75'}>
+        <div className={'h-20 w-20 bg-blue-500'}>Setting inner</div>
+      </div>
+    </>
+  );
+}
