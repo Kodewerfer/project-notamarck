1e830540a12585e08f1ff084cc5e6a69543e3e3a
Kodewerfer <lijleen@gmail.com>
2024-07-19 14:17:42 -0400

    renamed read md

--- src/routes/mainFrame._tabFrame.edit.$filepath.tsx
+++ src/routes/mainFrame._tabFrame.edit.$filepath.tsx
@@ -5,7 +5,7 @@ const { IPCRenderSide } = window;
 export const Route = createFileRoute('/mainFrame/_tabFrame/edit/$filepath')({
   // notify main of the new file, main process will then push all opened files to the tab frame
   loader: async ({ params: { filepath } }) => {
-    const OpenedFile = await IPCRenderSide.invoke(IPCActions.FILES.READ_MD_PATH, filepath);
+    const OpenedFile = await IPCRenderSide.invoke(IPCActions.FILES.READ_MD_FROM_PATH, filepath);
     IPCRenderSide.send(IPCActions.FILES.CHANGE_ACTIVE_FILE, OpenedFile);
     return OpenedFile;
   },
