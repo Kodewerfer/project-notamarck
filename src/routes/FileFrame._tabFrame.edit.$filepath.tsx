f26226430078b7440b5837f655425386a5f5c4e9
Kodewerfer <lijleen@gmail.com>
2024-08-20 22:41:07 -0400

    Add redirect if file is outside of workspace

--- src/routes/FileFrame._tabFrame.edit.$filepath.tsx
+++ src/routes/FileFrame._tabFrame.edit.$filepath.tsx
@@ -1,10 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import { IPCActions } from 'electron-src/IPC/IPC-Actions.ts';
+import { removeLastPartOfPath } from '@/util/helper.ts';
 
 const { IPCRenderSide } = window;
 export const Route = createFileRoute('/FileFrame/_tabFrame/edit/$filepath')({
   // notify main of the new file, main process will then push all opened files to the tab frame
   loader: async ({ params: { filepath } }) => {
+    const currentWorkspace = await IPCRenderSide.invoke(IPCActions.APP.GET_WORK_SPACE);
+    // check for the file's folder
+    if (removeLastPartOfPath(filepath) !== currentWorkspace) {
+      console.warn('Opened file not in workspace, redirected to index');
+      redirect({
+        to: '/FileFrame',
+      });
+      return;
+    }
     const OpenedFile = await IPCRenderSide.invoke(IPCActions.FILES.READ_MD_FROM_PATH, filepath);
     IPCRenderSide.send(IPCActions.DATA.CHANGE_ACTIVE_FILE, OpenedFile);
     return OpenedFile;
